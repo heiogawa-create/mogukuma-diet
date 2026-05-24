e0cbcb7a07397a987df4f75ecf611687568ec4e0
ヘイ <heiogawa@gmail.com>
2026-05-24 14:30:57 +0900

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,7 +5,7 @@ import Stripe from 'stripe';
 
 // Stripe インスタンス（サーバーサイドのみ）
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-04-10',
+  apiVersion: '2026-04-22.dahlia',
   typescript: true,
 });
 
